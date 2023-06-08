44debb51e34c7ab7ac187d3fb69aabc6d170ccef
Ragu Karuturi <ragukaruturi@microsoft.com>
2023-06-08 15:17:04 -0500

    setup

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import requests as rq
+import pandas as pd
+
+from flask import (Flask)
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+   print('Request on root')
+   return "Hello"
+
+
+@app.route('/test')
+def test():
+   print('test...')
+   return "test..."
+
+if __name__ == '__main__':
+   app.run()
